5fa21a15620390ea37fbda21267f44cdc26f51a8
𝐀𝐛𝐡𝐢𝐬𝐡𝐞𝐤 𝐏𝐚𝐭𝐞𝐥 <abhishekpatel4220@gmail.com>
2024-09-01 11:12:18 +0000

    css $ js complete

---
+++ script.js
@@ -0,0 +1,21 @@
+const yesButton = document.getElementById("yes");
+const noButton = document.getElementById("no");
+const cta = document.querySelector(".buttons");
+
+noButton.addEventListener("mouseover", () => {
+  const yesIndex = Array.from(cta.children).indexOf(yesButton);
+  const noIndex = Array.from(cta.children).indexOf(noButton);
+
+  if (yesIndex < noIndex) {
+    cta.insertBefore(noButton, yesButton);
+  } else {
+    cta.insertBefore(yesButton, noButton);
+  }
+});
+
+function yesfun() {
+  alert("magic see the button ha ha ha");
+}
+function nofun() {
+  alert("magic see the button ha ha ha ");
+}
